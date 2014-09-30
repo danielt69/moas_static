07ca3d96be874eb45a0e1470cbe714d61bb0314f
Daniel <danielt69@gmail.com>
2014-09-30 21:23:01 +0300

    store init

---
+++ Spree Demo Site_files/checkout.js
@@ -0,0 +1,12 @@
+(function() {
+  Spree.disableSaveOnClick = function() {
+    return ($('form.edit_order')).submit(function() {
+      return ($(this)).find(':submit, :image').attr('disabled', true).removeClass('primary').addClass('disabled');
+    });
+  };
+
+  Spree.ready(function($) {
+    return Spree.Checkout = {};
+  });
+
+}).call(this);
